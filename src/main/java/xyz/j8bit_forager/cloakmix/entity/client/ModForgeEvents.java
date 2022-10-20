9fa639fe7a6ce59a23354bd145865f6e1deb6a2d
J8-Bit <orangequilavaburst@gmail.com>
2022-10-20 12:30:20 -0700

    - Anonymity now pacifies Endermen

--- src/main/java/xyz/j8bit_forager/cloakmix/entity/client/ModForgeEvents.java
+++ src/main/java/xyz/j8bit_forager/cloakmix/entity/client/ModForgeEvents.java
@@ -4,18 +4,26 @@ import com.mojang.blaze3d.platform.InputConstants;
 import com.mojang.math.Vector3f;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.model.HumanoidModel;
+import net.minecraft.client.resources.sounds.AmbientSoundHandler;
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.network.chat.Component;
+import net.minecraft.sounds.SoundEvents;
 import net.minecraft.util.RandomSource;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.monster.EnderMan;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.enchantment.EnchantmentHelper;
+import net.minecraft.world.phys.AABB;
+import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.event.RenderLivingEvent;
 import net.minecraftforge.client.event.RenderNameTagEvent;
 import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.event.entity.living.EnderManAngerEvent;
+import net.minecraftforge.event.entity.living.LivingEquipmentChangeEvent;
 import net.minecraftforge.eventbus.api.Event;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -24,6 +32,8 @@ import xyz.j8bit_forager.cloakmix.enchantment.ModEnchantments;
 import xyz.j8bit_forager.cloakmix.item.custom.ModCloakItem;
 import xyz.j8bit_forager.cloakmix.item.custom.ModDyeableCloakItem;
 
+import javax.annotation.Nullable;
+import java.util.List;
 import java.util.Random;
 
 import static net.minecraft.util.Mth.*;
@@ -43,10 +53,10 @@ public class ModForgeEvents {
 
         if (event.getEntity() instanceof Player) {
 
-            Player player = (Player)event.getEntity();
+            Player player = (Player) event.getEntity();
             HumanoidModel model = (HumanoidModel) event.getRenderer().getModel();
 
-            if (player.isSprinting() && !player.isCrouching() && playerWearingCloak(player) && !player.isSwimming()) {
+            if (player.isSprinting() && !player.isCrouching() && !playerWearingCloak(player).isEmpty() && !player.isSwimming()) {
                 //event.getPoseStack().mulPose(Vector3f.ZP.rotationDegrees(180.0F));
 
 
@@ -55,10 +65,10 @@ public class ModForgeEvents {
 
                 model.getHead().setRotation(model.getHead().xRot, model.getHead().yRot - rotAmount, model.getHead().zRot);
 
-                event.getPoseStack().mulPose(Vector3f.ZP.rotationDegrees(rotAmount * sin( (float) Math.toRadians((double) yRot))));
-                event.getPoseStack().mulPose(Vector3f.XP.rotationDegrees(rotAmount * cos( (float) Math.toRadians((double) yRot))));
-                event.getPoseStack().translate(transAmount * cos( (float) Math.toRadians((double) yRot + 90.0)),
-                        0.0, transAmount * sin( (float) Math.toRadians((double) yRot + 90.0)));
+                event.getPoseStack().mulPose(Vector3f.ZP.rotationDegrees(rotAmount * sin((float) Math.toRadians((double) yRot))));
+                event.getPoseStack().mulPose(Vector3f.XP.rotationDegrees(rotAmount * cos((float) Math.toRadians((double) yRot))));
+                event.getPoseStack().translate(transAmount * cos((float) Math.toRadians((double) yRot + 90.0)),
+                        0.0, transAmount * sin((float) Math.toRadians((double) yRot + 90.0)));
 
             }
 
@@ -76,13 +86,13 @@ public class ModForgeEvents {
 
     @SubscribeEvent
     @OnlyIn(Dist.CLIENT)
-    public void onPlayerTick(TickEvent.PlayerTickEvent event){
+    public void onPlayerTick(TickEvent.PlayerTickEvent event) {
 
         // billowing
 
         ItemStack playerHelmet = event.player.getItemBySlot(EquipmentSlot.CHEST);
 
-        if (!playerHelmet.isEmpty()){
+        if (!playerHelmet.isEmpty()) {
 
             if (EnchantmentHelper.getTagEnchantmentLevel(ModEnchantments.BILLOWING.get(), playerHelmet) > 0) {
 
@@ -114,18 +124,18 @@ public class ModForgeEvents {
 
     @SubscribeEvent
     @OnlyIn(Dist.CLIENT)
-    public void onRenderTag(RenderNameTagEvent event){
+    public void onRenderTag(RenderNameTagEvent event) {
 
-        if (event.getEntity() instanceof Player){
+        if (event.getEntity() instanceof Player) {
 
-            ItemStack playerHelmet = ((Player)event.getEntity()).getItemBySlot(EquipmentSlot.CHEST);
+            ItemStack playerHelmet = ((Player) event.getEntity()).getItemBySlot(EquipmentSlot.CHEST);
 
             if (!playerHelmet.isEmpty()) {
 
                 if (EnchantmentHelper.getTagEnchantmentLevel(ModEnchantments.ANONYMITY.get(), playerHelmet) > 0) {
 
                     String replaceText = "";
-                    for (int i = 0; i < event.getContent().getString().length(); i++){
+                    for (int i = 0; i < event.getContent().getString().length(); i++) {
                         replaceText += "?";
                     }
 
@@ -143,10 +153,83 @@ public class ModForgeEvents {
 
     }
 
-    private boolean playerWearingCloak(Player p){
 
-        return (p.getItemBySlot(EquipmentSlot.CHEST).getItem() instanceof ModCloakItem
-        || p.getItemBySlot(EquipmentSlot.CHEST).getItem() instanceof ModDyeableCloakItem);
+    @SubscribeEvent
+    public void onEndermanAnger(EnderManAngerEvent event) {
+
+        if (playerWearingCloak(event.getPlayer()) != null){
+
+            if (EnchantmentHelper.getTagEnchantmentLevel(ModEnchantments.ANONYMITY.get(), playerWearingCloak(event.getPlayer())) > 0){
+
+                //CloakMix.LOGGER.info("DENY DENY DENY");
+
+                event.setCanceled(true);
+
+            }
+
+        }
+
+    }
+
+    @SubscribeEvent
+    @OnlyIn(Dist.CLIENT)
+    public void onArmorChange(LivingEquipmentChangeEvent event){
+
+        if (event.getEntity() instanceof Player){
+
+            if (event.getTo().getItem() instanceof ModCloakItem || event.getTo().getItem() instanceof ModDyeableCloakItem){
+
+                if (EnchantmentHelper.getTagEnchantmentLevel(ModEnchantments.ANONYMITY.get(), event.getTo()) > 0){
+
+                    double radius = 256.0;
+                    List<Entity> entities = event.getEntity().level.getEntities(event.getEntity(),
+                            new AABB(event.getEntity().blockPosition().offset(-radius/2.0, -radius/2.0, -radius/2.0), event.getEntity().blockPosition().offset(radius/2.0, radius/2.0, radius/2.0)));
+
+                    //int enderCount = 0;
+
+                    for (Entity entity:
+                         entities) {
+
+                        //CloakMix.LOGGER.info(entity.toString());
+
+                        if (entity instanceof EnderMan enderMan){
+
+                            //enderCount++;
+
+                            if (enderMan.getTarget() == event.getEntity()) {
+                                enderMan.setRemainingPersistentAngerTime(0);
+                                enderMan.setPersistentAngerTarget(null);
+                                enderMan.setTarget(null);
+                                enderMan.setAggressive(false);
+                                //entity.setDeltaMovement(0.0, 5.0, 0.0);
+
+                                Minecraft.getInstance().getSoundManager().stop(SoundEvents.ENDERMAN_STARE.getLocation(), enderMan.getSoundSource());
+                            }
+
+                        }
+
+                    }
+
+                    //CloakMix.LOGGER.info("There are " + enderCount + " Endermen");
+
+                }
+
+            }
+
+        }
+
+    }
+
+
+
+    @Nullable
+    private ItemStack playerWearingCloak(Player p) {
+
+       if (p.getItemBySlot(EquipmentSlot.CHEST).getItem() instanceof ModCloakItem
+                || p.getItemBySlot(EquipmentSlot.CHEST).getItem() instanceof ModDyeableCloakItem){
+           return p.getItemBySlot(EquipmentSlot.CHEST);
+       }
+       return null;
 
     }
 
